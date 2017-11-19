730b35615777677f3e26a422ccfc7812d5b2dbb6
siyixin <siyixin@sina.com>
2017-11-19 20:56:36 +0800

    地理插件

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('App', ['ionic'])
+angular.module('App', ['ionic','ngCordova'])
 
 .config(function ($stateProvider, $urlRouterProvider) {  
     $stateProvider
@@ -22,7 +22,13 @@ angular.module('App', ['ionic'])
   })
 
   .controller('LeftMenuController', function ($scope, Locations) {
-    $scope.locations = Locations.data;
+      $scope.locations = Locations.data;
+      $scope.getIcon = function (current) {
+          if (current) {
+              return 'ion-ios-navigate';
+          }
+          return 'ion-ios-location';
+      };
   })
 
   .factory('Settings', function () {
@@ -141,7 +147,7 @@ angular.module('App', ['ionic'])
     }
   })
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform,$cordovaGeolocation,$http,$state,Locations) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -149,5 +155,20 @@ angular.module('App', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+    $cordovaGeolocation.getCurrentPosition().then(function (data) {
+
+        $http.get('http://restapi.amap.com/v3/geocode/regeo', { params: { location: data.coords.longitude + ',' + data.coords.latitude, output: 'json', key: '40fc0b1b09a128854e51ddaf5dfd6a60' } }).success(function (response) {
+            var location = {
+                lat: data.coords.latitude,
+                lng: data.coords.longitude,
+                adcode: response.regeocode.addressComponent.adcode,
+                city: response.regeocode.formatted_address,
+                current: true
+            };
+            Locations.data.unshift(location);
+            $state.go('weather',location);
+        });
+    }, function (err) {console.log(err);},{timeout: 5000});
   });
 })
